ca63ff5775a4c127d8c7eab32368df6d88818900
Jamie Doornbos <jamie@threerings.net>
2012-06-06 21:56:44 -0700

    Test the use of mouse wheel listener on a layer.
    This fails to work and also crashes if the TestsGame global mouse adapter is commented out.

--- tests/core/src/main/java/playn/tests/core/TestsGame.java
+++ tests/core/src/main/java/playn/tests/core/TestsGame.java
@@ -38,6 +38,7 @@ public class TestsGame implements Game {
     new LayerClickTest(),
     new GetTextTest(),
     new PointerMouseTouchTest(),
+    new MouseWheelTest(),
     /*new YourTest(),*/
   };
   int currentTest;
